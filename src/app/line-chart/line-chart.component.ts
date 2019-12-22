e684997b4b29ce94abadd0983a9123cd689b07ac
4afS <g2x649@gmail.com>
2019-12-22 10:13:15 +0900

    define a function that draw Weight Chart

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -16,61 +16,67 @@ export class LineChartComponent implements AfterViewInit {
   @ViewChild('weightChart', {static: false})
   weightChart: any;
 
+  dates: string[];
+  weights: number[];
+
   ngAfterViewInit() {
     this.getWeightDataService.getWeightData().subscribe((jsonData: JsonData) => {
       const weightData: BodyWeight[] = jsonData.body_weight;
-      const dates = weightData.map(data => data.date);
-      const weights = weightData.map(data => data.weight);
-
+      this.dates = weightData.map(data => data.date);
+      this.weights = weightData.map(data => data.weight);
       this.context = this.weightChart.nativeElement.getContext('2d');
-      this.weightChart = new Chart(this.context, {
-        type: 'line',
-        data: {
-          labels: dates,
-          datasets: [{
-            label: 'Weight',
-            lineTension: 0,
-            pointRadius: 7,
-            pointBorderColor: 'white',
-            pointBorderWidth: 2,
-            spanGaps: true,
-            cubicInterpolationMode: 'monotone',
-            backgroundColor: 'rgba(0,121,107 ,0.5)',
-            borderColor: 'rgba(0,121,107 ,1)',
-            data: weights,
-            fill: true,
-          }]
+      this.weightChart = this.drawWeightChart();
+    });
+  }
+
+  private drawWeightChart() {
+    return new Chart(this.context, {
+      type: 'line',
+      data: {
+        labels: this.dates,
+        datasets: [{
+          label: 'Weight',
+          lineTension: 0,
+          pointRadius: 7,
+          pointBorderColor: 'white',
+          pointBorderWidth: 2,
+          spanGaps: true,
+          cubicInterpolationMode: 'monotone',
+          backgroundColor: 'rgba(0,121,107 ,0.5)',
+          borderColor: 'rgba(0,121,107 ,1)',
+          data: this.weights,
+          fill: true,
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltips: {
+          mode: 'index',
+          intersect: false,
         },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          tooltips: {
-            mode: 'index',
-            intersect: false,
-          },
-          title: {
-            fontStyle: 'bold',
-            fontSize: 60
-          },
-          scales: {
-            xAxes: [{
-              type: 'category',
-              position: 'button',
-              scaleLabel: {
-                labelString: 'Date',
-                display: false,
-              }
-            }],
-            yAxes: [{
-              type: 'linear',
-              scaleLabel: {
-                labelString: 'Weight',
-                display: false
-              }
-            }]
-          }
+        title: {
+          fontStyle: 'bold',
+          fontSize: 60
+        },
+        scales: {
+          xAxes: [{
+            type: 'category',
+            position: 'button',
+            scaleLabel: {
+              labelString: 'Date',
+              display: false,
+            }
+          }],
+          yAxes: [{
+            type: 'linear',
+            scaleLabel: {
+              labelString: 'Weight',
+              display: false
+            }
+          }]
         }
-      });
+      }
     });
   }
 }
